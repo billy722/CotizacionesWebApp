10f97b0245831e20af7b0e33958f1860f3ba02b1
Billy Salazar <billysalazar@MBP-de-Billy.lan>
2023-03-10 23:45:54 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CotizacionesComponent } from './cotizaciones/cotizaciones.component';
+import { DetalleCotizacionComponent } from './detalle-cotizacion/detalle-cotizacion.component';
+import { ProductosComponent } from './productos/productos.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
+import { ClientesComponent } from './clientes/clientes.component';
+
+const appRoutes: Routes=[
+  {
+    path:"",
+    component: ClientesComponent
+  },
+  {
+    path:"cotizaciones",
+    component: CotizacionesComponent
+  },
+  {
+    path:"detalle_cotizacion",
+    component: DetalleCotizacionComponent
+  },
+  {
+    path:"productos",
+    component: ProductosComponent
+  },
+  {
+    path:"detalle_producto",
+    component: DetalleProductoComponent
+  },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CotizacionesComponent,
+    DetalleCotizacionComponent,
+    ProductosComponent,
+    DetalleProductoComponent,
+    ClientesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/detalle-cotizacion/detalle-cotizacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-cotizacion',
+  templateUrl: './detalle-cotizacion.component.html',
+  styleUrls: ['./detalle-cotizacion.component.css']
+})
+export class DetalleCotizacionComponent {
+
+}
